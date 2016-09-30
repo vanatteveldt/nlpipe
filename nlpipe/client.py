2f77dd03c93561006cbb600b635d6574f71e50f9
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-09-30 11:13:52 +0200

    Partial implementation of file based client

--- nlpipe/client.py
+++ nlpipe/client.py
@@ -1,6 +1,24 @@
 import time
+import os.path
+from hashlib import md5
 
-#TODO: how do we want to signal failure? In the document?
+import errno
+
+STATUS = {"queue": "PENDING", "inprogress": "STARTED", "result": "DONE", "error": "ERROR"}
+
+
+def get_id(doc):
+    """
+    Calculate the id (hash) of the given document
+    :param doc: The document (string)
+    :return: a task id (hash)
+    """
+    if len(doc) == 34 and doc.startswith("0x"):
+        # it sure looks like a hash
+        return doc
+    m = md5.new()
+    m.update(doc)
+    return "0x" + m.hexdigest()
 
 
 class Client(object):
@@ -10,11 +28,11 @@ class Client(object):
         """Add a document to be processed by module, returning the task ID"""
         raise NotImplementedError()
 
-    def done(self, module, doc):
+    def status(self, module, doc):
         """Query whether a document has been processed, by document or by task ID
         :param module: Module name
         :param doc: A document (string) or task ID
-        :return: Boolean indicating whether processing is done
+        :return: any of 'UNKNOWN', 'PENDING', 'STARTED', 'DONE', 'ERROR'
         """
         raise NotImplementedError()
 
@@ -33,11 +51,15 @@ class Client(object):
         :param doc: A document (string) or task ID
         :return: The result of processing (string)
         """
-        if not self.done(module, doc):
+        if self.status(module, doc) == 'UNKNOWN':
             self.process(module, doc)
-        while not self.done(module, doc):
+        while True:
+            status = self.status(module, doc)
+            if status == 'DONE':
+                return self.result(module, doc)
+            if status == 'ERROR':
+                raise Exception("Error on processing {doc} with {module}".format(**locals()))
             time.sleep(0.1)
-        return self.result(module, doc)
 
     def get_task(self, module):
         """
@@ -66,6 +88,7 @@ class Client(object):
         """
         raise NotImplementedError()
 
+
 class FSClient(Client):
     """
     NLPipe client that relies on direct filesystem access (e.g. on local machine or over NFS)
@@ -74,11 +97,39 @@ class FSClient(Client):
     def __init__(self, result_dir):
         self.result_dir = result_dir
 
-    def process(self, module, doc):
+    def _check_dirs(self, module):
+        for subdir in STATUS.keys():
+            dirname = os.path.join(self.result_dir, module, subdir)
+            try:
+                os.makedirs(dirname)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+
+    def _write(self, module, subdir, doc, id=None):
         self._check_dirs(module)
+        if id is None:
+            id = get_id(doc)
+        fn = os.path.join(self.result_dir, module, subdir, id)
+        open(fn, 'w').write(doc)
+        return fn
 
+    def _read(self, module, subdir, doc):
+        id = get_id(doc)
+        fn = os.path.join(self.result_dir, module, subdir, id)
+        return open(fn).read()
 
+    def status(self, module, doc):
+        id = get_id(doc)
+        for subdir, status in STATUS.items():
+            if os.path.exists(os.path.join(self.result_dir, module, subdir, id)):
+                return STATUS
+        return 'UNKNOWN'
 
+    def process(self, module, doc):
+        id = get_id(doc)
+        if self.status(module, id) == 'UNKNOWN':
+            self._write(module, 'queue', doc, id)
 
-
-
+    def result(self, module, doc):
+        return self._read(module, 'results', doc)
