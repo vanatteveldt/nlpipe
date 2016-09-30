4ed2e86a3589d688139b68b99ea0eddfc53f8774
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-09-30 19:43:47 +0200

    Travis setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="nlpipe",
+    version="0.4",
+    description="Simple NLP Pipelinining based on a file system",
+    author="Wouter van Atteveldt",
+    author_email="wouter@vanatteveldt.com",
+    packages=["nlpipe"],
+    keywords = ["NLP", "pipelining"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Text Processing",
+    ],
+    install_requires=[]
+)
