540895ba5dd47e55f36d041b410075f57e3c417f
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-10-12 17:30:33 +0200

    Fix worker test

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -4,30 +4,25 @@ import time
 from nose.tools import assert_equal, assert_true, assert_false
 
 from nlpipe.client import FSClient
-from nlpipe.worker import SystemWorker, FunctionWorker
-
+from nlpipe.worker import Worker
+from nlpipe.modules.test_upper import TestUpper
 
 SYSUPPER = "tr '[:lower:]' '[:upper:]'"
 FNUPPER = lambda x: x.upper()
 
-def test_process():
-    w = SystemWorker(None, "test", SYSUPPER)
-    assert_equal(w.process("test"), "TEST")
-    w = FunctionWorker(None, "test", FNUPPER)
-    assert_equal(w.process("test"), "TEST")
-
 def test_worker():
     with TemporaryDirectory() as dir:
         c = FSClient(dir)
-        w = SystemWorker(c, "test", SYSUPPER)
+        m = TestUpper()
+        w = Worker(c, m)
 
-        id = c.process(w.module_name, "test")
-        assert_equal(c.status(w.module_name, id), "PENDING")
+        id = c.process(m.name, "test")
+        assert_equal(c.status(m.name, id), "PENDING")
 
         w.start()
         time.sleep(0.2)
 
-        assert_equal(c.status(w.module_name, id), "DONE")
-        assert_equal(c.result(w.module_name, id), "TEST")
+        assert_equal(c.status(m.name, id), "DONE")
+        assert_equal(c.result(m.name, id), "TEST")
 
         w.terminate()
