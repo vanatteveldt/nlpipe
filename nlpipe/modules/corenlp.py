4361448ed4ea05a8114439c8986fe66d496c6471
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-10-12 17:24:54 +0200

    Make corenlp host configurable via env

--- nlpipe/modules/corenlp.py
+++ nlpipe/modules/corenlp.py
@@ -1,7 +1,8 @@
 """
 Wrapper around the CoreNLP server (http://nlp.stanford.edu/software/corenlp.shtml)
 
-Assumes a CoreNLP server is listening at port 9000, i.e. run:
+Assumes a CoreNLP server is listening at CORENLP_HOST (default localhost:9000)
+E.g. you can run:
 docker run -dp 9000:9000 chilland/corenlp-docker
 """
 
@@ -9,12 +10,15 @@ from nlpipe.module import Module
 from urllib.parse import urlencode
 import requests
 import json
+import os
 
 class CoreNLPLemmatizer(object):
     name = "corenlp_lemmatize"
     properties = {"annotators": "tokenize,ssplit,pos,lemma,ner", "outputFormat": "xml"}
 
-    def __init__(self, server="http://localhost:9000"):
+    def __init__(self, server=None):
+        if server is None:
+            server = os.getenv('CORENLP_HOST', 'http://localhost:9000')
         self.server = server
 
     def status(self):
@@ -27,5 +31,5 @@ class CoreNLPLemmatizer(object):
         url = "{self.server}/?{query}".format(**locals())
         res = requests.post(url, data=text)
         if res.status_code != 200:
-            raise Exception("Error calling corenlp: {res.status_code}\n{res.content}".format(**locals()))
+            raise Exception("Error calling corenlp at {url}: {res.status_code}\n{res.content}".format(**locals()))
         return res.content.decode("utf-8")
