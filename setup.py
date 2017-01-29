c5b658a550c6cc2e2f2e8ef0e3f72c1fafed817e
Wouter van Atteveldt <wouter@vanatteveldt.com>
2017-01-29 13:01:40 +0800

    fix application name (me=stupid)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="nlpipe",
-    version="0.42",
+    version="0.43",
     description="Simple NLP Pipelinining based on a file system",
     author="Wouter van Atteveldt",
     author_email="wouter@vanatteveldt.com",
